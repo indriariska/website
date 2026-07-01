0646d30de743fce0859c01e78fb6c4fd80f70223
indriariska <indriariska469@gmail.com>
2026-07-01 18:53:40 +0700

    update

--- cv-business - Copy/src/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-const Response = require('../utils/response');
-
-const notFound = (req, res, next) => {
-  return Response.error(res, `Route ${req.originalUrl} not found`, 404);
-};
-
-module.exports = notFound;
--- cv-business - Copy/src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DashboardController = require('../controllers/dashboardController');
-const { auth, staffOrAdmin } = require('../middleware/auth');
-
-// Both admin and staff can view dashboard stats
-router.get('/stats', auth, staffOrAdmin, DashboardController.getStats);
-
-module.exports = router;
